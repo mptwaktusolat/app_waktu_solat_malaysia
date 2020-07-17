20dd73bb159d537a120597febdb9cc143fb3362b
iqFareez <60868965+fareezMaple@users.noreply.github.com>
2020-07-18 01:42:54 +0800

    messing up a litte bit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:waktusolatmalaysia/sizeconfig.dart';
+import 'package:waktusolatmalaysia/utils/sizeconfig.dart';
 
 void main() {
   runApp(MyApp());
---
+++ lib/models/azanproapi.dart
@@ -0,0 +1,81 @@
+class PrayerTime {
+  String zone;
+  String start;
+  String end;
+  List<String> locations;
+  PrayerTimes prayerTimes;
+
+  PrayerTime(
+      {this.zone, this.start, this.end, this.locations, this.prayerTimes});
+
+  PrayerTime.fromJson(Map<String, dynamic> json) {
+    zone = json['zone'];
+    start = json['start'];
+    end = json['end'];
+    locations = json['locations'].cast<String>();
+    prayerTimes = json['prayer_times'] != null
+        ? new PrayerTimes.fromJson(json['prayer_times'])
+        : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['zone'] = this.zone;
+    data['start'] = this.start;
+    data['end'] = this.end;
+    data['locations'] = this.locations;
+    if (this.prayerTimes != null) {
+      data['prayer_times'] = this.prayerTimes.toJson();
+    }
+    return data;
+  }
+}
+
+class PrayerTimes {
+  String date;
+  int datestamp;
+  int imsak;
+  int subuh;
+  int syuruk;
+  int zohor;
+  int asar;
+  int maghrib;
+  int isyak;
+
+  PrayerTimes(
+      {this.date,
+      this.datestamp,
+      this.imsak,
+      this.subuh,
+      this.syuruk,
+      this.zohor,
+      this.asar,
+      this.maghrib,
+      this.isyak});
+
+  PrayerTimes.fromJson(Map<String, dynamic> json) {
+    date = json['date'];
+    datestamp = json['datestamp'];
+    imsak = json['imsak'];
+    subuh = json['subuh'];
+    syuruk = json['syuruk'];
+    zohor = json['zohor'];
+    asar = json['asar'];
+    maghrib = json['maghrib'];
+    isyak = json['isyak'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['date'] = this.date;
+    data['datestamp'] = this.datestamp;
+    data['imsak'] = this.imsak;
+    data['subuh'] = this.subuh;
+    data['syuruk'] = this.syuruk;
+    data['zohor'] = this.zohor;
+    data['asar'] = this.asar;
+    data['maghrib'] = this.maghrib;
+    data['isyak'] = this.isyak;
+    return data;
+  }
+}
