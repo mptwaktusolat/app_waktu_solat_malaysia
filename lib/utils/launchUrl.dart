4d7b8568bc75de1e97e6dd2759869291c0061d7d
iqFareez <60868965+fareezMaple@users.noreply.github.com>
2020-08-30 11:39:19 +0800

    :heavy_plus_sign: Added launchUrl

---
+++ lib/utils/launchUrl.dart
@@ -0,0 +1,10 @@
+class LaunchUrl {
+  void normalLaunchUrl() {
+    print('Called normal laucnh');
+  }
+
+  void customTabsUrl() {
+    print('Called custom tabs');
+    //TODO: Custom tabs
+  }
+}
