276a9fae1938199a33df9cd9232ed6f28d4fa5b5
iqFareez <60868965+fareezMaple@users.noreply.github.com>
2020-10-06 12:53:42 +0800

    :heavy_minus_sign: Removed bloc and flutter_bloc :heavy_plus_sign: Added provider

--- lib/views/Settings part/ThemePage.dart
+++ lib/views/Settings part/ThemePage.dart
@@ -1,51 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:waktusolatmalaysia/blocs/change_theme_bloc.dart';
-import 'package:waktusolatmalaysia/views/Settings%20part/change_theme_state.dart';
 
-//partly adapted from https://gist.github.com/bimsina/a04ac08358b4d1553ef2498d9155c9c1#file-awesome_widget-dart
-
-class ThemesPage extends StatefulWidget {
-  @override
-  _ThemesPageState createState() => _ThemesPageState();
-}
-
-class _ThemesPageState extends State<ThemesPage> {
+class ThemesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Themes'),
+        title: Text('Theme'),
+        centerTitle: true,
       ),
-      body: Container(
-        child: ThemesOptions(),
-      ),
-    );
-  }
-}
-
-class ThemesOptions extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return BlocBuilder<ChangeThemeBloc, ChangeThemeState>(
-      builder: (context, state) {
-        return ListView(
-          children: [
-            RadioListTile(
-                value: 'null',
-                groupValue: 'null',
-                onChanged: (_) {
-                  changeThemeBloc.onLightThemeChange();
-                }),
-            RadioListTile(
-                value: 'null',
-                groupValue: 'null',
-                onChanged: (_) {
-                  print('Hello world');
-                }),
-          ],
-        );
-      },
+      body: Container(),
     );
   }
 }
