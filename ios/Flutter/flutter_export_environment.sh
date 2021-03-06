6b0810d6e9cdd18f4c7a2b0141fab78048187ef5
iqfareez <iqmal3@outlook.com>
2021-03-07 00:30:39 +0800

    :rocket: 1.0.4-web+5

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\Iqmal\fvm\versions\beta"
-export "FLUTTER_APPLICATION_PATH=D:\Iqmal\Documents\Flutter apps\App-Waktu-Solat-Malaysia"
+export "FLUTTER_ROOT=C:\Users\Iqmal\fvm\default"
+export "FLUTTER_APPLICATION_PATH=D:\Iqmal\Documents\Flutter apps\app_waktu_solat_malaysia"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "FLUTTER_BUILD_NAME=1.0.2"
-export "FLUTTER_BUILD_NUMBER=3"
+export "FLUTTER_BUILD_NAME=1.0.4"
+export "FLUTTER_BUILD_NUMBER=5"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=false"
 export "TREE_SHAKE_ICONS=false"
