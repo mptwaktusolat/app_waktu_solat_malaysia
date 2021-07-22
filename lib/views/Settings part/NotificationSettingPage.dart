bcafcb31d6351666c2b860ef1974b9779052042b
iqfareez <iqmal3@outlook.com>
2021-07-22 12:56:45 +0800

    :fire: Removed unused notification UI

--- lib/views/Settings part/NotificationSettingPage.dart
+++ lib/views/Settings part/NotificationSettingPage.dart
@@ -5,7 +5,7 @@ import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import '../../CONSTANTS.dart';
 import '../../utils/cupertinoSwitchListTile.dart';
-import '../../utils/navigator_pop.dart';
+import '../../utils/custom_navigator_pop.dart';
 
 class NotificationPageSetting extends StatefulWidget {
   const NotificationPageSetting({Key key}) : super(key: key);
@@ -15,95 +15,16 @@ class NotificationPageSetting extends StatefulWidget {
 }
 
 class _NotificationPageSettingState extends State<NotificationPageSetting> {
-  var prayerNotification = [true, true, true, true, true, true, true];
-  bool isAnythingChanged = false; //effect the button
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Notification'),
         centerTitle: true,
-        // actions: [
-        //   FlatButton(
-        //     shape: CircleBorder(side: BorderSide(color: Colors.transparent)),
-        //     textColor: Colors.white,
-        //     onPressed: !isAnythingChanged
-        //         ? null
-        //         : () {
-        //             print('Save button clicked');
-        //             print(prayerNotification);
-        //             Navigator.pop(context);
-        //           },
-        //     child: Text(
-        //       'SAVE',
-        //     ),
-        //   )
-        // ],
       ),
       body: ListView(
         padding: const EdgeInsets.all(16),
         children: [
-          //Turned of for a while
-          // Text('Basic'),
-          // Card(
-          //   child: Column(children: [
-          //     CupertinoSwitchListTile(
-          //         title: Text(PrayerName.prayerName[0]),
-          //         value: prayerNotification[0],
-          //         onChanged: (bool value) {
-          //           setState(() {
-          //             isAnythingChanged = true;
-          //             prayerNotification[0] = value;
-          //           });
-          //         }),
-          //     CupertinoSwitchListTile(
-          //         title: Text(PrayerName.prayerName[1]),
-          //         value: prayerNotification[1],
-          //         onChanged: (bool value) {
-          //           setState(() {
-          //             isAnythingChanged = true;
-          //             prayerNotification[1] = value;
-          //           });
-          //         }),
-          //     CupertinoSwitchListTile(
-          //         title: Text(PrayerName.prayerName[2]),
-          //         value: prayerNotification[2],
-          //         onChanged: (bool value) {
-          //           setState(() {
-          //             isAnythingChanged = true;
-          //             prayerNotification[2] = value;
-          //           });
-          //         }),
-          //     CupertinoSwitchListTile(
-          //         title: Text(PrayerName.prayerName[3]),
-          //         value: prayerNotification[3],
-          //         onChanged: (bool value) {
-          //           setState(() {
-          //             isAnythingChanged = true;
-          //             prayerNotification[3] = value;
-          //           });
-          //         }),
-          //     CupertinoSwitchListTile(
-          //         title: Text(PrayerName.prayerName[4]),
-          //         value: prayerNotification[4],
-          //         onChanged: (bool value) {
-          //           setState(() {
-          //             isAnythingChanged = true;
-          //             prayerNotification[4] = value;
-          //           });
-          //         }),
-          //     CupertinoSwitchListTile(
-          //         title: Text(PrayerName.prayerName[5]),
-          //         value: prayerNotification[5],
-          //         onChanged: (bool value) {
-          //           setState(() {
-          //             isAnythingChanged = true;
-          //             prayerNotification[5] = value;
-          //           });
-          //         }),
-          //   ]),
-          // ),
           const Padding(padding: EdgeInsets.all(8.0), child: Text('Basic')),
           Card(
             child: ListTile(
@@ -120,7 +41,6 @@ class _NotificationPageSettingState extends State<NotificationPageSetting> {
           ),
           const Padding(
               padding: EdgeInsets.all(8.0), child: Text('Troubleshooting')),
-
           Card(
             child: Container(
               padding: const EdgeInsets.symmetric(vertical: 8.0),
@@ -130,7 +50,6 @@ class _NotificationPageSettingState extends State<NotificationPageSetting> {
                       'Enable if you experiencing an extreme slowdown in app. Notification will schedule weekly basis. Default is OFF (monthly).'),
                   value: GetStorage().read(kStoredNotificationLimit),
                   onChanged: (value) {
-                    // print(value);
                     setState(() {
                       GetStorage().write(kStoredNotificationLimit, value);
                     });
