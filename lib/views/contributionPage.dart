4fcf0d69b16dd96b2fc49d836802df30d218ca62
iqfareez <iqmal3@outlook.com>
2022-01-18 08:20:09 +0800

    :globe_with_meridians: Done last part maybe #90
    In contribution page

--- lib/views/contributionPage.dart
+++ lib/views/contributionPage.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:share_plus/share_plus.dart';
+
 import '../CONSTANTS.dart' as constants;
 import '../utils/launchUrl.dart';
 
@@ -18,7 +20,7 @@ class ContributionPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Contribution and Support'),
+        title: Text(AppLocalizations.of(context)!.aboutContribution),
         centerTitle: true,
       ),
       body: SingleChildScrollView(
@@ -26,57 +28,62 @@ class ContributionPage extends StatelessWidget {
           padding: const EdgeInsets.all(16.0),
           child: Column(
             children: [
-              const Text(
-                  'Alhamdulillah. Thank you for your interest in donating to the Malaysia Prayer Time app. May Allah SWT rewards your kindness.'),
-              const SizedBox(
-                height: 8,
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Text(AppLocalizations.of(context)!.contributenDesc),
               ),
+              const SizedBox(height: 8),
               MyCard(
-                  title: 'Share the app',
+                  title: AppLocalizations.of(context)!.contributeShare,
                   description:
-                      'Share your experience on using this app with your family and friends.',
+                      AppLocalizations.of(context)!.contributeShareDesc,
                   buttonContent: [
-                    ButtonContent('Share now', () {
+                    ButtonContent(
+                        AppLocalizations.of(context)!.contributeShareNow, () {
                       Share.share(
-                          "Hi. I'm using the Malaysia Prayer Time app. It's fast and free.\nTry it now:\n${constants.kPlayStoreListingShortLink} (Google Play)\n ${constants.kWebappUrl} (Web app)",
-                          subject: 'Sharing MPT App');
+                          AppLocalizations.of(context)!.contributeShareContent(
+                              constants.kPlayStoreListingShortLink,
+                              constants.kWebappUrl),
+                          subject: AppLocalizations.of(context)!
+                              .contributeShareSubject);
                     })
                   ]),
               const Divider(),
               MyCard(
-                title: 'Buy me a coffee?',
-                description:
-                    'One cup of Nescafe is usually enough for me to code all night.\n\n${constants.kBuyMeACoffeeLink.substring(12)}', //substring will remove 'https://www' stuffs.
+                title: AppLocalizations.of(context)!.contributeCoffee,
+                description: AppLocalizations.of(context)!.contributeCoffeeDesc(
+                    constants.kBuyMeACoffeeLink
+                        .replaceFirst("https://www.", "")),
                 buttonContent: [
                   ButtonContent(
-                    'Copy',
-                    () => copyClipboard(constants.kBuyMeACoffeeLink),
+                    AppLocalizations.of(context)!.contributeCopy,
+                    () => copyClipboard(context, constants.kBuyMeACoffeeLink),
                   ),
-                  ButtonContent('Open', () {
+                  ButtonContent(AppLocalizations.of(context)!.contributeOpen,
+                      () {
                     LaunchUrl.normalLaunchUrl(url: constants.kBuyMeACoffeeLink);
                   })
                 ],
               ),
               MyCard(
-                title: 'Direct support',
+                title: AppLocalizations.of(context)!.contributeDirect,
                 description:
                     '${constants.kBankAccountNum} - Muhammad Fareez Iqmal (Maybank)',
                 buttonContent: [
                   ButtonContent(
-                    'Copy',
-                    () => copyClipboard(constants.kBankAccountNum),
+                    AppLocalizations.of(context)!.contributeCopy,
+                    () => copyClipboard(context, constants.kBankAccountNum),
                   ),
                 ],
               ),
               MyCard(
-                title: 'Contribute to source',
-                description:
-                    'MPT is open source. Report any bugs or contribute directly to the source code. It is licensed under GNU GPLv3.',
+                title: AppLocalizations.of(context)!.contributeSource,
+                description: AppLocalizations.of(context)!.contributeSourceDesc,
                 buttonContent: [
+                  ButtonContent(AppLocalizations.of(context)!.contributeCopy,
+                      () => copyClipboard(context, constants.kGithubRepoLink)),
                   ButtonContent(
-                      'Copy', () => copyClipboard(constants.kGithubRepoLink)),
-                  ButtonContent(
-                      'Open GitHub',
+                      AppLocalizations.of(context)!.contributeOpenGh,
                       () => LaunchUrl.normalLaunchUrl(
                           url: constants.kGithubRepoLink)),
                 ],
@@ -95,9 +102,10 @@ class ContributionPage extends StatelessWidget {
     );
   }
 
-  void copyClipboard(String url) {
-    Clipboard.setData(ClipboardData(text: url))
-        .then((value) => Fluttertoast.showToast(msg: 'Copied to clipboard'));
+  void copyClipboard(BuildContext context, String url) {
+    Clipboard.setData(ClipboardData(text: url)).then((value) =>
+        Fluttertoast.showToast(
+            msg: AppLocalizations.of(context)!.contributeCopied));
   }
 }
 
