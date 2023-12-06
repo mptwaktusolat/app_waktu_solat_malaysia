c1544fa3bf62b745acaad651c0415490a6588ae4
Fareez Iqmal <60868965+iqfareez@users.noreply.github.com>
2023-12-07 07:02:03 +0800

    :sparkles: Make widget update at midnight

--- android/app/src/main/java/live/iqfareez/waktusolatmalaysia/SolatHorizontalWidget.kt
+++ android/app/src/main/java/live/iqfareez/waktusolatmalaysia/SolatHorizontalWidget.kt
@@ -1,29 +1,41 @@
 package live.iqfareez.waktusolatmalaysia
 
+import android.app.AlarmManager
 import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
+import android.content.ComponentName
 import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
+import android.os.Build
 import android.widget.RemoteViews
-import es.antonborri.home_widget.HomeWidgetBackgroundIntent
-import es.antonborri.home_widget.HomeWidgetLaunchIntent
-import es.antonborri.home_widget.HomeWidgetProvider
+import es.antonborri.home_widget.HomeWidgetPlugin
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
+import java.util.Locale
+import java.util.TimeZone
+
+
+private const val ACTION_SCHEDULED_UPDATE = "live.iqfareez.waktusolatmalaysia.SCHEDULED_UPDATE"
 
 /**
  * Implementation of App Widget functionality.
  */
-class SolatHorizontalWidget : HomeWidgetProvider() {
+class SolatHorizontalWidget : AppWidgetProvider() {
     override fun onUpdate(
         context: Context,
         appWidgetManager: AppWidgetManager,
         appWidgetIds: IntArray,
-        widgetData: SharedPreferences
     ) {
+        val widgetData = HomeWidgetPlugin.getData(context)
         // There may be multiple widgets active, so update all of them
         for (appWidgetId in appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId, widgetData)
         }
+
+        scheduleNextUpdate(context);
     }
 
     override fun onEnabled(context: Context) {
@@ -32,6 +44,15 @@ class SolatHorizontalWidget : HomeWidgetProvider() {
 
     override fun onDisabled(context: Context) {
         // Enter relevant functionality for when the last widget is disabled
+    }
+
+    override fun onReceive(context: Context, intent: Intent) {
+        super.onReceive(context, intent);
+        if (intent.action.equals(ACTION_SCHEDULED_UPDATE)) {
+            val manager = AppWidgetManager.getInstance(context)
+            val ids = manager.getAppWidgetIds(ComponentName(context, SolatHorizontalWidget::class.java))
+            onUpdate(context, manager, ids)
+        }
     }
 }
 
@@ -45,26 +66,88 @@ internal fun updateAppWidget(
     val views = RemoteViews(context.packageName, R.layout.solat_horizontal_widget)
 
     val launchIntent = context.packageManager.getLaunchIntentForPackage(context.packageName)
-    val pendingIntent = PendingIntent.getActivity(context, 0, launchIntent,
-        PendingIntent.FLAG_IMMUTABLE)
+    val pendingIntent = PendingIntent.getActivity(
+        context, 0, launchIntent,
+        PendingIntent.FLAG_IMMUTABLE
+    )
 
     // Set the click listener for the widget
     views.setOnClickPendingIntent(android.R.id.background, pendingIntent)
 
-    // Set widget content
-    views.setTextViewText(R.id.widget_title, widgetData.getString("widget_title", null)
-        ?: "Please open app to set widget data")
-    views.setTextViewText(R.id.subuh_time, widgetData.getString("widget_subuh_time", null)
-        ?: "00:00")
-    views.setTextViewText(R.id.zuhur_time, widgetData.getString("widget_zuhur_time", null)
-        ?: "00:00")
-    views.setTextViewText(R.id.asar_time, widgetData.getString("widget_asar_time", null)
-        ?: "00:00")
-    views.setTextViewText(R.id.maghrib_time, widgetData.getString("widget_maghrib_time", null)
-        ?: "00:00")
-    views.setTextViewText(R.id.isyak_time, widgetData.getString("widget_isyak_time", null)
-        ?: "00:00")
+    val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+    dateFormat.timeZone = TimeZone.getTimeZone("Asia/Kuala_Lumpur") // Set Malaysia timezone
+    val formattedDate = dateFormat.format(Date())
+
+    views.setTextViewText(R.id.widget_date, formattedDate)
+
+    //  Set widget content
+    views.setTextViewText(
+        R.id.widget_title, widgetData.getString("widget_title", null)
+            ?: "Please open app to set widget data"
+    )
+    views.setTextViewText(
+        R.id.subuh_time, widgetData.getString("widget_subuh_time", null)
+            ?: "00:00"
+    )
+    views.setTextViewText(
+        R.id.zuhur_time, widgetData.getString("widget_zuhur_time", null)
+            ?: "00:00"
+    )
+    views.setTextViewText(
+        R.id.asar_time, widgetData.getString("widget_asar_time", null)
+            ?: "00:00"
+    )
+    views.setTextViewText(
+        R.id.maghrib_time, widgetData.getString("widget_maghrib_time", null)
+            ?: "00:00"
+    )
+    views.setTextViewText(
+        R.id.isyak_time, widgetData.getString("widget_isyak_time", null)
+            ?: "00:00"
+    )
 
     // Instruct the widget manager to update the widget
     appWidgetManager.updateAppWidget(appWidgetId, views)
+}
+
+// Credit to: https://stackoverflow.com/a/37901697/13617136
+private fun scheduleNextUpdate(context: Context) {
+    val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+    // Substitute AppWidget for whatever you named your AppWidgetProvider subclass
+    val intent = Intent(context, SolatHorizontalWidget::class.java)
+    intent.setAction(ACTION_SCHEDULED_UPDATE)
+    val pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+
+    // Get a calendar instance for midnight tomorrow.
+    val midnight: Calendar = Calendar.getInstance()
+    midnight.set(Calendar.HOUR_OF_DAY, 0)
+    midnight.set(Calendar.MINUTE, 0)
+    // Schedule one second after midnight, to be sure we are in the right day next time this
+    // method is called.  Otherwise, we risk calling onUpdate multiple times within a few
+    // milliseconds
+    midnight.set(Calendar.SECOND, 1)
+    midnight.set(Calendar.MILLISECOND, 0)
+    midnight.add(Calendar.DAY_OF_YEAR, 1)
+
+    // For API 19 and later, set may fire the intent a little later to save battery,
+    // setExact ensures the intent goes off exactly at midnight.
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
+        alarmManager[AlarmManager.RTC_WAKEUP, midnight.getTimeInMillis()] = pendingIntent
+    } else {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            if (alarmManager.canScheduleExactAlarms()) {
+                alarmManager.setExact(
+                    AlarmManager.RTC_WAKEUP,
+                    midnight.timeInMillis,
+                    pendingIntent
+                )
+            } else {
+                alarmManager.setExact(
+                    AlarmManager.RTC_WAKEUP,
+                    midnight.timeInMillis,
+                    pendingIntent
+                )
+            }
+        }
+    }
 }
