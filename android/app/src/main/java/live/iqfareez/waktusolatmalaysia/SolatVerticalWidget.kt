a74befd17b3d5ab57a4e3ac01dd41df326ce394d
Fareez Iqmal <60868965+iqfareez@users.noreply.github.com>
2023-12-09 09:20:38 +0800

    :lipstick: Add vertical layout widget

---
+++ android/app/src/main/java/live/iqfareez/waktusolatmalaysia/SolatVerticalWidget.kt
@@ -0,0 +1,42 @@
+package live.iqfareez.waktusolatmalaysia
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.widget.RemoteViews
+
+/**
+ * Implementation of App Widget functionality.
+ */
+class SolatVerticalWidget : AppWidgetProvider() {
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        // There may be multiple widgets active, so update all of them
+        for (appWidgetId in appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId)
+        }
+    }
+
+    override fun onEnabled(context: Context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    override fun onDisabled(context: Context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
+internal fun updateAppWidget(
+    context: Context,
+    appWidgetManager: AppWidgetManager,
+    appWidgetId: Int
+) {
+    // Construct the RemoteViews object
+    val views = RemoteViews(context.packageName, R.layout.solat_vertical_widget)
+
+    // Instruct the widget manager to update the widget
+    appWidgetManager.updateAppWidget(appWidgetId, views)
+}
