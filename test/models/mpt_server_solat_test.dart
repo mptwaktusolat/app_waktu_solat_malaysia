733d85929aaef0d99bb1dc7a7db799bd389b2b16
iqfareez <60868965+iqfareez@users.noreply.github.com>
2025-02-16 16:36:54 +0800

    :recycle: Update API response test data and remove unused widget test

--- test/models/mpt_server_solat_test.dart
+++ test/models/mpt_server_solat_test.dart
@@ -2,54 +2,54 @@ import 'dart:convert';
 
 import 'package:flutter_test/flutter_test.dart';
 import 'package:waktusolatmalaysia/models/mpt_server_solat.dart';
+import 'package:waktusolatmalaysia/utils/hijri_date.dart';
 
 void main() {
   test('Parse API response', () async {
-    const jsonString =
-        '''
+    // The string below based on https://api.waktusolat.app/v2/solat/{zone}
+    // The [prayers] array is shortened for brevity.
+    const jsonString = '''
 {
-    "zone": "SGR01",
-    "year": 2023,
-    "month": "MAY",
-    "last_updated": "2023-05-18T18:48:11.892Z",
-    "prayers": [
-        {
-            "syuruk": 1682895720,
-            "dhuhr": 1682917980,
-            "maghrib": 1682940000,
-            "hijri": "1444-10-10",
-            "isha": 1682944260,
-            "fajr": 1682891640,
-            "day": 1,
-            "asr": 1682929860
-        },
-        {
-            "maghrib": 1683026400,
-            "dhuhr": 1683004380,
-            "hijri": "1444-10-11",
-            "day": 2,
-            "isha": 1683030660,
-            "fajr": 1682977980,
-            "asr": 1683016260,
-            "syuruk": 1682982120
-        },
-    ]
+  "zone": "SGR01",
+  "year": 2025,
+  "month": "FEB",
+  "last_updated": "2025-01-15T03:01:06.095Z",
+  "prayers": [
+    {
+      "isha": 1738413600,
+      "day": 1,
+      "syuruk": 1738365960,
+      "dhuhr": 1738387800,
+      "hijri": "1446-08-02",
+      "asr": 1738399860,
+      "fajr": 1738361880,
+      "maghrib": 1738409280
+    },
+    {
+      "dhuhr": 1738474200,
+      "maghrib": 1738495680,
+      "day": 2,
+      "asr": 1738486260,
+      "hijri": "1446-08-03",
+      "syuruk": 1738452360,
+      "isha": 1738500000,
+      "fajr": 1738448280
+    }
+  ]
 }
 ''';
     final json = jsonDecode(jsonString);
     final solatData = MptServerSolat.fromJson(json);
 
     expect(solatData.zone, 'SGR01');
-    expect(solatData.year, 2023);
-    expect(solatData.month, 'MAY');
-    expect(solatData.prayers.length, 2);
+    expect(solatData.year, 2025);
+    expect(solatData.month, 'FEB');
 
     final firstPrayer = solatData.prayers[0];
-    expect(firstPrayer.day, 1);
-    expect(firstPrayer.fajr, DateTime(2023, 5, 1, 5, 54));
+
+    expect(firstPrayer.fajr, DateTime(2025, 2, 1, 6, 18));
 
     final secondPrayer = solatData.prayers[1];
-    expect(secondPrayer.day, 2);
-    expect(secondPrayer.hijri, '1444-10-11');
+    expect(secondPrayer.hijri, HijriDate.parse('1446-08-03'));
   });
 }
