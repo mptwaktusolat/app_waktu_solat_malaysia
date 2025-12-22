2c4189e293f9b18d13053859d8edda5f892cca34
fareez <iqmal3@outlook.com>
2025-12-23 07:31:22 +0800

    🚨 remove unused variables

--- android/app/src/main/java/live/iqfareez/waktusolatmalaysia/ui/theme/Theme.kt
+++ android/app/src/main/java/live/iqfareez/waktusolatmalaysia/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package live.iqfareez.waktusolatmalaysia.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
